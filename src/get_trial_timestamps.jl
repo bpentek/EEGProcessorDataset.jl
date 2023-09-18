045adc7c315f1539325cfb676daef55917d6a42a
bpentek <pentekabalazs@gmail.com>
2023-09-18 18:42:01 +0300

    fixed error in function `get_trial_timestamps`

--- src/get_trial_timestamps.jl
+++ src/get_trial_timestamps.jl
@@ -1,14 +1,14 @@
 # returns timestamps of the input trial
-function get_trial_timestamps(epd::EPD; start_code::I, end_code::I, trial_nr::I)::Tuple{I, I} where I <: Integer
+function get_trial_timestamps(epd::EPD; start_code::I, end_code::I, trial_nr::I, trial_start_code::I=128)::Tuple{I, I} where I <: Integer
 
     events = get_events(epd)
-    start_idx = findall(x -> x == start_code, events[:, 2])[trial_nr]
-    end_idx = findall(x -> x == end_code, events[:, 2])[trial_nr]
+    trial_start = findall(x -> x == trial_start_code, events[:, 2])[trial_nr]
+    start_idx = findnext(x -> x == start_code, events[:, 2], trial_start)
+    end_idx = findnext(x -> x == end_code, events[:, 2], trial_start)
     
     if events[start_idx, 1] > events[end_idx, 1]
         throw("ERROR: for codes `$(start_code)` and `$(end_code)` in trial nr. $(trial_nr): starting timestamp ($(events[start_idx, 1])) > ending timestamp ($(events[end_idx, 1]))")
     end
-
 
     return (events[start_idx, 1], events[end_idx, 1])
 
