2544d3793739bd11f235e046b960ae597502143b
rusandris <rusandris@gmail.com>
2023-09-25 11:27:43 +0300

    minor bugfix in bad event code error checking

--- src/get_trial_timestamps.jl
+++ src/get_trial_timestamps.jl
@@ -10,7 +10,7 @@ function get_trial_timestamps(epd::EPD; start_code::I, end_code::I, trial_nr::I,
     
     next_trial_start = findnext(x -> x == trial_start_code, events[:, 2], trial_start+1)
     
-    if isnothing(end_idx) || end_idx > next_trial_start
+    if isnothing(end_idx) || (!isnothing(next_trial_start) && next_trial_start < end_idx)
     	throw("ERROR: End code `$(end_code)` in trial nr. $(trial_nr) does not exist!")
     end
     
